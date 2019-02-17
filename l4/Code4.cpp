94576a7821462b4f3788ecb3576e9ad9cc0a0335
AndLvovSky <viktorilvovskyi@ukr.net>
2019-02-17 19:57:03 +0200

    Update lab 4

--- l4/Code4.cpp
+++ l4/Code4.cpp
@@ -7,8 +7,11 @@ using namespace std;
 
 const int INF = 1000 * 1000 * 1000;
 
+// дерево бінарного пошуку
 template <typename T>
 class BinarySearchTree {
+	// l, r - вказівники на ліве і праве піддерево 
+	// key - значення у вузлі
 	struct Node {
 		Node *l, *r;
 		T key;
@@ -16,20 +19,29 @@ class BinarySearchTree {
 		Node(T key = T(), Node* l = NULL, Node* r = NULL) : key(key), l(l), r(l) {
 		}
 	};
+	// корінь
 	Node* root;
+	// сума добутків частот на глибини відповідних значень (вартість)
 	int cost;
 	
 	// побудувати оптимальне дерево на основі частот frs і відповідних ключів keys 
-	// на для ключів від l до r
+	// для ключів від l до r та/або обчислити його найменшу вартість
+	// dp[l][r] = 0, якщо дана функція не викликалась з даними параметрами, інакше - це 
+	// найменша вартість бінарного дерева, побудованого з ключів від l до r включно
+	// frs, keys - частоти і відповідні ключі
+	// функція повертає побудоване оптимальне дерево або нульовий вказівник
 	Node* buildOptimalDP(int l, int r, bool create, vector < vector <int> >& dp, vector <int>& frs, vector <T>& keys) {
+		// дерево складається з одного вузла
 		if (l == r) {
 			dp[l][r] = frs[l];
 			return create ? new Node(keys[l], NULL, NULL) : NULL;
 		}
+		// обчислюємо суму частот
 		int s = 0;
 		for (int i = l; i <= r; i++) {
 			s += frs[i];
 		}
+		// знаходимо оптимальну вартість та вершину, яку найбільш вигідно зробити корнем
 		int mn = INF, mni = l;
 		for (int i = l; i <= r; i++) {
 			if (i != l && !dp[l][i - 1]) {
@@ -44,17 +56,20 @@ class BinarySearchTree {
 				mni = i;
 			}
 		}
+		// застосовуємо рекурентну формулу
 		dp[l][r] = mn + s;
 		if (!create) {
 			return NULL;
 		}
+		// будуємо дерево та вертаємо вказівник на корінь, якщо необхідно
 		Node* node = new Node(keys[mni]);
 		node->l = l == mni ? NULL : buildOptimalDP(l, mni - 1, true, dp, frs, keys);
 		node->r = r == mni ? NULL : buildOptimalDP(mni + 1, r, true, dp, frs, keys);
 		return node;
 	}
 	
-	// побудувати оптимальне дерево на основі частот frs і відповідних ключів keys 
+	// побудувати оптимальне дерево на основі частот frs і відповідних ключів keys
+	// функція повертає мінімальну вартість дерева, яку можна досягти
 	int buildOptimal(vector <int>& frs, vector <T>& keys) {
 		vector < vector <int> > dp(frs.size());
 		for (auto& v : dp) {
@@ -76,10 +91,11 @@ class BinarySearchTree {
 	}
 	
 public:
-	
+	// конструктор 1
 	BinarySearchTree() : root(NULL) {
 	}	
 	
+	// конструктор 2
 	BinarySearchTree(vector <int> frs, vector <T> keys) : BinarySearchTree() {
 		cost = buildOptimal(frs, keys);
 	}
@@ -100,14 +116,15 @@ class CompanyDivision {
 	friend void read(); 
 	
 public:
-	
+	// рядкове представлення компанії
 	operator string() {
 		return name;
 	}
 };
 
-// список пар вигляду: частота звертання до підрозділу - сам підрозділ
 typedef pair <CompanyDivision, int> CInfo;
+
+// список пар вигляду: частота звертання до підрозділу - сам підрозділ
 vector <CInfo> compDivsInfo; 
 
 // зчитування підрозділів компанії та їх апріорних частот
@@ -123,6 +140,7 @@ void read() {
 	}
 }
 
+// тестування
 int main() {
 	read();
 	// сортування підрозділів за збільшенням частоти
@@ -134,6 +152,7 @@ int main() {
 		compDivs.push_back(it.first);
 		frs.push_back(it.second);
 	}
+	// побудова оптимального дереве бінарного пошуку на основі частот і ключів
 	BinarySearchTree <CompanyDivision> bst(frs, compDivs);
 	bst.show();
 }
